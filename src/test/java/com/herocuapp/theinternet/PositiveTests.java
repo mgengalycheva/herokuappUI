2d893d6447d78ae9a42d8eede56a72f50e02d272
mgengalycheva <mgengalycheva@gmail.com>
2021-09-15 12:53:44 +0200

    adding verifications in positive test

--- src/test/java/com/herocuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herocuapp/theinternet/PositiveTests.java
@@ -1,7 +1,10 @@
 package com.herocuapp.theinternet;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PositiveTests {
@@ -22,17 +25,35 @@ public class PositiveTests {
         driver.get(url);
         System.out.println("Page is opened");
 
-        sleep(3000);
-
 // enter username
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+
+// enter password
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecretPassword!");
+
+// click login button
+        WebElement loginButton = driver.findElement(By.xpath("//button[contains(@type, 'submit')]"));
+        loginButton.click();
+
+// verification:
+// new url
+        String expectedUrl = "http://the-internet.herokuapp.com/secure";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl, "Actual page url is not the same as expected");
 
 
-//        enter password
-//        click login button
-//        verification:
-//        new url
-//        logout button is visible
-//        successful login message
+// logout button is visible
+        WebElement logOutButton = driver.findElement(By.xpath("//a[@class='button secondary radius']"));
+        Assert.assertTrue(logOutButton.isDisplayed(), "Logout button is not visible");
+
+// successful login message
+        WebElement successMessage = driver.findElement(By.cssSelector("#flash"));
+        String expectedMessage = "You logged into a secure area!";
+        String actualMessage = successMessage.getText();
+        //Assert.assertEquals(actualMessage, expectedMessage, "Actual message is not the same as expected");
+        Assert.assertTrue(actualMessage.contains(expectedMessage), "Actual message is not the same as expected");
 
 
 // close browser
