98a640186a7e93cb48b572243b3d26fdffb47456
mgengalycheva <mgengalycheva@gmail.com>
2021-09-22 10:58:55 +0200

    adding suites for test

---
+++ src/test/java/com/herocuapp/theinternet/NegativeTests.java
@@ -0,0 +1,86 @@
+package com.herocuapp.theinternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NegativeTests {
+
+
+    @Test(priority = 1, groups = { "negativeTests", "smokeTests"})
+    public void incorrectUserNameTest() {
+
+        System.out.println("Starting incorrectUserNameTest");
+
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        String url = "http://the-internet.herokuapp.com/login";
+        driver.get(url);
+        System.out.println("Page is opened");
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("incorrect");
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecretPassword!");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[contains(@type, 'submit')]"));
+        loginButton.click();
+
+        //verification
+
+        WebElement notSuccessMessage = driver.findElement(By.cssSelector("#flash"));
+        String expectedMessage = "Your username is invalid!";
+        String actualMessage = notSuccessMessage.getText();
+
+        Assert.assertTrue(actualMessage.contains(expectedMessage), "Actual message is not the same as expected");
+
+        //
+
+        driver.quit();
+
+    }
+
+    @Test(priority = 2, enabled = false, groups = { "negativeTests"})
+    public void incorrectPasswordTest() {
+
+        System.out.println("Starting incorrectPasswordTest");
+
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        String url = "http://the-internet.herokuapp.com/login";
+        driver.get(url);
+        System.out.println("Page is opened");
+
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("incorrectPassword");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[contains(@type, 'submit')]"));
+        loginButton.click();
+
+        //verification
+
+        WebElement notSuccessMessage = driver.findElement(By.cssSelector("#flash"));
+        String expectedMessage = "Your password is invalid!";
+        String actualMessage = notSuccessMessage.getText();
+
+        Assert.assertTrue(actualMessage.contains(expectedMessage), "Actual message is not the same as expected");
+
+        //
+
+        driver.quit();
+
+    }
+}
